caf815862fd9c195bfe66cf3b5c6c1ca3d54be3f
taco <derpytacozlol@gmail.com>
2016-11-12 09:16:04 -0500

    mkvendor.sh: Initial commit of pop35

---
+++ AndroidProducts.mk
@@ -0,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/device_pop35.mk
---
+++ cm.mk
@@ -0,0 +1,15 @@
+# Release name
+PRODUCT_RELEASE_NAME := pop35
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/alcatel/pop35/device_pop35.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := pop35
+PRODUCT_NAME := cm_pop35
+PRODUCT_BRAND := alcatel
+PRODUCT_MODEL := pop35
+PRODUCT_MANUFACTURER := alcatel
